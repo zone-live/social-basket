fe65d7626534a153e104aec5aee3c75f1b3e977f
António Regadas <antonio.regadas@premium-minds.com>
2017-02-28 13:59:26 +0000

    addSocial funcion in progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Fund }    from './fund';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    newFund: any;
+    funds: any;
+    fundObj: any;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.newFund = '';
+        this.funds = [];
+    }
 
+    ngOnInit() {
+    }
+
+    addFund(event) {
+        this.fundObj = {
+            newFund: this.newFund
+        }
+        this.funds.push(this.fundObj);
+        this.newFund = '';
+        event.preventDefault();
+    }
+
+    //model = new Fund('isin', 'name', 'subs_date', 2, 31, 10 );
+
+    //get diagnostic() { return JSON.stringify(this.model); }
 }
