dbcfa4fa3a7350aa460092a16968539fd44da20d
António Regadas <antonio.regadas@premium-minds.com>
2017-03-01 18:53:19 +0000

    math done

---
+++ src/app/home/fund.interface.ts
@@ -0,0 +1,15 @@
+export interface Fund {
+    name: string,
+    isin: string,
+    subs_date: string,
+    up_number: number,
+    up_quote: number,
+    value_bought: number,
+    up_price: number,
+    at_date: string,
+    todays_value: number,
+    earnings: number,
+    gross_proff: number,
+    liquid_proff: number,
+    days_passed: number
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Fund }    from './fund';
+import { Fund } from './fund.interface';
 
 @Component({
   selector: 'app-home',
@@ -8,29 +8,77 @@ import { Fund }    from './fund';
 })
 
 export class HomeComponent implements OnInit {
-
-    newFund: any;
-    funds: any;
-    fundObj: any;
+    public fund : Fund;
+    public funds: any;
+    public valueBought;
+    public todaysValue;
 
     constructor() {
-        this.newFund = '';
         this.funds = [];
     }
 
     ngOnInit() {
-    }
-
-    addFund(event) {
-        this.fundObj = {
-            newFund: this.newFund
+        this.fund = {
+            name: '',
+            isin: '',
+            subs_date: '',
+            up_number: 0,
+            up_quote: 0,
+            value_bought: 0,
+            up_price: 0,
+            at_date: '',
+            todays_value: 0,
+            earnings: 0,
+            gross_proff: 0,
+            liquid_proff: 0,
+            days_passed: 0
         }
-        this.funds.push(this.fundObj);
-        this.newFund = '';
-        event.preventDefault();
     }
 
-    //model = new Fund('isin', 'name', 'subs_date', 2, 31, 10 );
+    getValueBought(up_number, up_quote) {
+        this.valueBought = Number(up_number) * Number(up_quote);
+        console.log(this.valueBought);
+        this.funds.push(this.valueBought);
+    }
 
+    getEarnings(todays_value, value_bought) {
+        console.log('todays_value: ' + todays_value);
+        console.log('value_bought: ' + value_bought);
+        console.log('todays_value - value_bought = ' + (todays_value - value_bought));
+        return todays_value - value_bought;
+    }
+
+    getTodaysValue(up_price, up_number) {
+        this.todaysValue = Number(up_price) * Number(up_number);
+        console.log(this.todaysValue);
+        this.funds.push(this.todaysValue);
+    }
+
+    getGrossProff(todays_value, value_bought) {
+        console.log(todays_value + ' ' + value_bought);
+        console.log(this.getEarnings(todays_value, value_bought) / value_bought);
+        return this.getEarnings(todays_value, value_bought) / value_bought;
+    }
+
+    getLiquidProff(todays_value, value_bought) {
+        console.log(todays_value + ' ' + value_bought);
+        console.log((this.getEarnings(todays_value, value_bought)*0.72) / value_bought);
+        return (this.getEarnings(todays_value, value_bought)*0.72) / value_bought;
+    }
+
+    getDaysPassed(subs_date, at_date) {
+        return subs_date - at_date;
+    }
+
+    addFund(submitFund, event) {
+        console.log(submitFund);
+        event.preventDefault();
+        this.getValueBought(submitFund.up_number, submitFund.up_quote)
+        this.getTodaysValue(submitFund.up_price, submitFund.up_number)
+        this.getEarnings(this.todaysValue, this.valueBought)
+        this.getGrossProff(this.todaysValue, this.valueBought)
+        this.getLiquidProff(this.todaysValue, this.valueBought)
+    }
+    //model = new Fund('isin', 'name', 'subs_date', 2, 31, 10 );
     //get diagnostic() { return JSON.stringify(this.model); }
 }
