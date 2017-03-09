86f13dfe3e08b902d1456785ccf74feeffa8b9e4
António Regadas <antonio.regadas@premium-minds.com>
2017-03-09 21:36:50 +0000

    fixed variable name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var tasks = require('./routes/funds');
+var funds = require('./routes/funds');
 
 var app = express();
 
