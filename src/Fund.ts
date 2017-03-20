5d4b43255393c92223c1c54f5841a8a4859c80d8
António Regadas <antonio.regadas@premium-minds.com>
2017-03-20 17:58:46 +0000

    updated fund class

--- src/Fund.ts
+++ src/Fund.ts
@@ -1,16 +1,16 @@
 export class Fund {
-    _id: String,
-    name: String,
-    isin: String,
-    subs_date: String,
-    up_number: Number,
-    up_quote: Number,
-    value_bought: Number,
-    up_price: Number,
-    at_date: String,
-    todays_value: Number,
-    earnings: Number,
-    gross_proff: Number,
-    liquid_proff: Number,
+    _id: String;
+    name: String;
+    isin: String;
+    subs_date: String;
+    up_number: Number;
+    up_quote: Number;
+    value_bought: Number;
+    up_price: Number;
+    at_date: String;
+    todays_value: Number;
+    earnings: Number;
+    gross_proff: Number;
+    liquid_proff: Number;
     days_passed: Number
 }
